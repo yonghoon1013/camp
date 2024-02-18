01bf43f5871e16c50c4385b0d2b6f448fe5f6d2e
yonghoon1013 <jyh10105@gmail.com>
2024-02-18 23:17:40 +0900

    zz

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -53,15 +53,15 @@ const KakaoMap: React.FC<DataComponentProps> = ({ props }) => {
             }
         };
 
-        if (!window.kakao) {
+        if (!window.kakao || !window.kakao.maps) {
             const script = document.createElement("script");
             script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_API_KEY}&autoload=false`;
             script.async = true;
-            document.head.appendChild(script);
 
             script.onload = () => {
                 window.kakao.maps.load(loadKakaoMap);
             };
+            document.head.appendChild(script);
         } else {
             loadKakaoMap();
         }
